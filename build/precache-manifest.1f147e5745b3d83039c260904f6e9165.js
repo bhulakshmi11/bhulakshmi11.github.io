9b892e818f07bf13eea205aa6608ad30d88bdb93
Bhulakshmi <bhulakshmi@bitlab.academy>
2018-11-13 16:43:59 +0530

    Initial commit

---
+++ build/precache-manifest.1f147e5745b3d83039c260904f6e9165.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d6dce5ce04d7e1aa791f5ed31d17e34",
+    "url": "/static/media/download.5d6dce5c.svg"
+  },
+  {
+    "revision": "90bab3867ec1549cc535",
+    "url": "/static/css/main.dbfdba0d.chunk.css"
+  },
+  {
+    "revision": "82bed902eaeb22481586",
+    "url": "/static/js/1.82bed902.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4359369bd29c6dbaa4d95081bfa50a5e",
+    "url": "/static/media/gitt.4359369b.svg"
+  },
+  {
+    "revision": "a5f3289fb14af881568d6d3b121dbd19",
+    "url": "/static/media/hand.a5f3289f.svg"
+  },
+  {
+    "revision": "6ed7131f3530ad9a5954b1e151cab22a",
+    "url": "/static/media/tag.6ed7131f.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21baf6a9e90cdb9accccc60eedc6ef94",
+    "url": "/static/media/mailbox.21baf6a9.svg"
+  },
+  {
+    "revision": "f52dae36303b554490c8be1c5c243542",
+    "url": "/static/media/fax.f52dae36.svg"
+  },
+  {
+    "revision": "d6afe6cc8291f4aa291356da47f56312",
+    "url": "/static/media/hello.d6afe6cc.svg"
+  },
+  {
+    "revision": "963060d9e78665ad81a3d4f5c71f2a1c",
+    "url": "/static/media/cap.963060d9.svg"
+  },
+  {
+    "revision": "90bab3867ec1549cc535",
+    "url": "/static/js/main.90bab386.chunk.js"
+  },
+  {
+    "revision": "be47f1a62f5179fd7b55d3e0ceb396ae",
+    "url": "/static/media/git.be47f1a6.svg"
+  },
+  {
+    "revision": "7a2efc4b416f004e7bf2af9b062a955c",
+    "url": "/static/media/clipboard.7a2efc4b.svg"
+  },
+  {
+    "revision": "b2b26bbed3095a54712556274db5a53b",
+    "url": "/static/media/arrow.b2b26bbe.svg"
+  },
+  {
+    "revision": "cd99c0f2f875972ca4e16e76c1b2961c",
+    "url": "/static/media/fpage.cd99c0f2.svg"
+  },
+  {
+    "revision": "958532ebc1a46894d04c59c70d75600f",
+    "url": "/static/media/fo.958532eb.svg"
+  },
+  {
+    "revision": "c6ff5bc43d04fd7b59edf58afc5434d8",
+    "url": "/static/media/ffpage.c6ff5bc4.svg"
+  },
+  {
+    "revision": "6ead6153f3df2c76261fc2ecbc68c2d2",
+    "url": "/static/media/mail.6ead6153.svg"
+  },
+  {
+    "revision": "3d59acacdf408d7ae794cfbcb16c64ad",
+    "url": "/static/media/in.3d59acac.svg"
+  },
+  {
+    "revision": "b5c6297f431838039560464eeec3a0dc",
+    "url": "/static/media/fb.b5c6297f.svg"
+  },
+  {
+    "revision": "203e960cf7042a8756a640bb137816d2",
+    "url": "/static/media/twitt.203e960c.svg"
+  },
+  {
+    "revision": "e7abcbffb07b1e2be78d903bb2f26ed4",
+    "url": "/static/media/hifi.e7abcbff.svg"
+  },
+  {
+    "revision": "8c317a0983a7955a73969a6fae1c6a84",
+    "url": "/index.html"
+  }
+];
